4e7700ccbfc16b3448a0cfa436e5798f845a5a37
Joseph Vargas <134672039+Joseph1799@users.noreply.github.com>
2023-10-15 20:35:09 -0600

    Update email_attachment_downloader.py

--- email_attachment_downloader.py
+++ email_attachment_downloader.py
@@ -2,34 +2,34 @@ from simplegmail import Gmail
 from simplegmail.query import construct_query
 import os
 
-print("Current working directory:", os.getcwd())
-
+# Inicia una conexión a Gmail.
 gmail = Gmail()
 
-# Consulta para obtener correos actuales etiquetados como "PIMA".
+# Configuración de la consulta para obtener correos electrónicos etiquetados como "Tu_Etiqueta_Aquí".
 query_params = {
-    "label": "Tu_Etiquieta_Aquí",
+    "label": "Tu_Etiqueta_Aquí",  # Modifica "Tu_Etiqueta_Aquí" por la etiqueta deseada.
 }
 
+# Obtiene los mensajes que coinciden con la consulta.
 messages = gmail.get_messages(query=construct_query(query_params))
 
-# Ordenar los mensajes por fecha de manera descendente (los más recientes primero).
+# Ordena los mensajes por fecha de manera descendente (los más recientes primero).
 messages.sort(key=lambda message: message.date, reverse=True)
 
+# Si se encuentran mensajes:
 if messages:
-    latest_message = messages[0]  # El primer mensaje en la lista es el más reciente.
+    # Obtiene el mensaje más reciente de la lista.
+    latest_message = messages[0]
     print("To: " + latest_message.recipient)
     print("From: " + latest_message.sender)
     print("Subject: " + latest_message.subject)
     print("Date: " + latest_message.date)
     print("Preview: " + latest_message.snippet)
 
-    # Verificar si el correo electrónico contiene un archivo PDF adjunto.
+    # Verifica si el correo electrónico contiene un archivo PDF adjunto.
     for attachment in latest_message.attachments:
         if attachment.filename.lower().endswith('.pdf'):
-            # Guardar el archivo PDF adjunto en la ubicación deseada.
+            # Guarda el archivo PDF adjunto en la ubicación deseada.
             attachment.save('C:\\Users\\user\\dir\\dir\\dir\\' + attachment.filename, overwrite=True)
-
-
 else:
-    print("No se encontraron correos electrónicos en la etiqueta 'Tu_Etiquieta_Aquí'.")
+    print("No se encontraron correos electrónicos en la etiqueta 'Tu_Etiqueta_Aquí'.")
